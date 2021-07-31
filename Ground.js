b99b10358f2eedc4acefabcc755e2fa884ab6051
Snigdhadipm <85871995+Snigdhadipm@users.noreply.github.com>
2021-07-31 16:07:52 +0530

    Add files via upload

---
+++ Ground.js
@@ -0,0 +1,10 @@
+class Ground{
+    constructor(x,y,width) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+    }
+    display() {
+        groundObj= new ground(width/2,670,width,20);
+    }
+}
